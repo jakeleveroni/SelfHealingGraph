87394bcdfd4e49a7fed12466ead142c7ff02aceb
Jacob <jakeleveroni@yahoo.com>
2017-12-02 11:47:30 -0800

    removed template, added timing statistics and utility, added logging and graph generation

---
+++ SelfHealingNetwork/Structures/TimingStatistic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SelfHealingNetwork.Structures
+{
+    public class TimingStatistic
+    {
+        private DateTime _startTime;
+        private DateTime _endTime;
+
+        public struct NodeInfo
+        {
+            public string NodeName;
+            public int NumberOfEdges;
+        }
+        
+        public TimeSpan ElapsedTime => _endTime - _startTime;
+
+        public void Start()
+        {
+            _startTime = DateTime.Now;
+        }
+
+        public void Stop()
+        {
+            _endTime = DateTime.Now;
+        }
+    }
+}
---
+++ SelfHealingNetwork/Utility.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SelfHealingNetwork
+{
+    public static class Utility
+    {
+        private static string _nodeVals= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        public static List<NodeValue> GetPossibleNodeValues()
+        {
+            return _nodeVals.Select(letter => new NodeValue(letter)).ToList();
+        }
+
+        public static NodeValue Next(this List<NodeValue> l)
+        {
+            var rng = new Random();
+
+            while (true)
+            {
+                var index = rng.Next(0, l.Count);
+
+                if (l[index].Taken) continue;
+                
+                var val = l[index];
+                l.RemoveAt(index);
+                return val;
+            }
+        }
+    }
+
+    public class NodeValue
+    {
+        public readonly char Value;
+        public readonly bool Taken;
+
+        public NodeValue(char val)
+        {
+            Value = val;
+            Taken = false;
+        }
+    }
+}
